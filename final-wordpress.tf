1c2ff858c9d299ab5eee74fce78c2387ada84325
skhedim <46813029+skhedim@users.noreply.github.com>
2021-01-28 09:49:37 +0100

    Update final-wordpress.tf

--- final-wordpress.tf
+++ final-wordpress.tf
@@ -1,70 +1,69 @@
 provider "aws" {
-  region = "us-east-1"
+  region ="us-east-1"
 }
 
-resource "aws_vpc" "default" {
-  cidr_block       = "10.0.0.0/16"
-
+resource "aws_vpc" "epsi-tf" {
+  cidr_block = "10.0.0.0/16"
+  
   tags = {
-    Name = "cesi-tf"
+    Name = "epsi-tf"
   }
 }
 
 resource "aws_subnet" "public-a" {
-  vpc_id     = aws_vpc.default.id
-  availability_zone = "us-east-1a"
+  vpc_id     = aws_vpc.epsi-tf.id
   cidr_block = "10.0.1.0/24"
-
+  availability_zone = "us-east-1a"
+  
   tags = {
     Name = "public-a-tf"
   }
 }
 
 resource "aws_subnet" "public-b" {
-  vpc_id     = aws_vpc.default.id
-  availability_zone = "us-east-1b"
+  vpc_id     = aws_vpc.epsi-tf.id
   cidr_block = "10.0.2.0/24"
-
+  availability_zone = "us-east-1b"
+  
   tags = {
     Name = "public-b-tf"
   }
 }
 
 resource "aws_subnet" "private-a" {
-  vpc_id     = aws_vpc.default.id
-  availability_zone = "us-east-1a"
+  vpc_id     = aws_vpc.epsi-tf.id
   cidr_block = "10.0.3.0/24"
-
+  availability_zone = "us-east-1a"
+  
   tags = {
     Name = "private-a-tf"
   }
 }
 
 resource "aws_subnet" "private-b" {
-  vpc_id     = aws_vpc.default.id
-  availability_zone = "us-east-1b"
+  vpc_id     = aws_vpc.epsi-tf.id
   cidr_block = "10.0.4.0/24"
-
+  availability_zone = "us-east-1a"
+  
   tags = {
     Name = "private-b-tf"
   }
 }
 
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.default.id
+resource "aws_internet_gateway" "igw-tf" {
+  vpc_id = aws_vpc.epsi-tf.id
 
   tags = {
     Name = "igw-tf"
   }
 }
 
-resource "aws_route_table" "r" {
-  vpc_id = aws_vpc.default.id
-
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.epsi-tf.id
   route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
-  }
+  cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.igw-tf.id
+}
 
   tags = {
     Name = "public-tf"
@@ -72,47 +71,13 @@ resource "aws_route_table" "r" {
 }
 
 resource "aws_route_table_association" "a" {
-  subnet_id      = aws_subnet.public-a.id
-  route_table_id = aws_route_table.r.id
+  subnet_id = aws_subnet.public-a.id
+  route_table_id = aws_route_table.public.id
 }
 
 resource "aws_route_table_association" "b" {
-  subnet_id      = aws_subnet.public-b.id
-  route_table_id = aws_route_table.r.id
-}
-
-resource "tls_private_key" "example" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
-resource "aws_key_pair" "deployer" {
-  key_name   = "ec2-key-tf"
-  public_key = tls_private_key.example.public_key_openssh
-}
-
-resource "aws_security_group" "allow_http" {
-  name        = "allow_tls"
-  description = "Allow http inbound traffic"
-  vpc_id      = aws_vpc.default.id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  
-  egress {
-  from_port   = 0
-  to_port     = 0
-  protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "allow_http-tf"
-  }
+  subnet_id = aws_subnet.public-b.id
+  route_table_id = aws_route_table.public.id
 }
 
 data "aws_ami" "ubuntu" {
@@ -135,84 +100,48 @@ resource "aws_instance" "wordpress" {
     ami = data.aws_ami.ubuntu.id
     instance_type = "t2.micro"
     vpc_security_group_ids = [aws_security_group.allow_http.id]
-    key_name = "ec2-key-cesi2"
+    key_name = aws_key_pair.deployer.key_name
     subnet_id     = aws_subnet.public-a.id
     associate_public_ip_address = true
 
     tags = {
         Name = "wordpress"
     }
-    user_data = templatefile("${path.root}/postinstall.sh", {
-      password = random_password.dbpassword.result
-      endpoint = aws_db_instance.dbWordPress.address
-    })
-}
-
-resource "random_password" "dbpassword" {
-  length = 16
-  special = true
-  override_special = "_%@"
-}
-
-resource "aws_security_group" "allow_rds" {
-  name        = "allow_rds"
-  description = "Allow mysql inbound traffic"
-  vpc_id      = aws_vpc.default.id
+    
+    user_data = file("${path.root}/wordpress.sh")
+  }
+  
+  resource "aws_security_group" "allow_http" {
+  name        = "allow_http"
+  description = "Allow http inbound traffic"
+  vpc_id      = aws_vpc.epsi-tf.id
 
   ingress {
-    from_port   = 3306
-    to_port     = 3306
+    description = "HTTP from VPC"
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
-    security_groups = [aws_security_group.allow_http.id]
+    cidr_blocks = ["0.0.0.0/0"]
   }
-  
+
   egress {
-  from_port   = 0
-  to_port     = 0
-  protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
-    Name = "allow_http-tf"
+    Name = "allow_http"
   }
 }
 
-resource "aws_db_subnet_group" "rds" {
-  name       = "wordpress"
-  subnet_ids = [aws_subnet.private-a.id, aws_subnet.private-b.id]
-
-  tags = {
-    Name = "wordpress-rds"
-  }
+resource "tls_private_key" "example" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
 }
 
-resource "aws_db_instance" "dbWordPress" {
-  engine = "mysql"
-  engine_version = "5.7"
-  allocated_storage = 20
-  instance_class = "db.t2.micro"
-  vpc_security_group_ids = [aws_security_group.allow_rds.id]
-  db_subnet_group_name = aws_db_subnet_group.rds.name
-  name = "wordpress"
-  username = "admin"
-  password = random_password.dbpassword.result
-  parameter_group_name = "default.mysql5.7"
-  skip_final_snapshot = true
-  
-  tags = {
-      Name = "WordPress DB"
-  }
-}
-
-output "private-key" {
-  value = tls_private_key.example.private_key_pem
-}
-
-output "public-ip" {
-  value = aws_instance.wordpress.public_ip
-}
-
-output "rds_password" {
-  value = random_password.dbpassword.result
+resource "aws_key_pair" "deployer" {
+  key_name   = "ec2-key-tf"
+  public_key = tls_private_key.example.public_key_openssh
 }
