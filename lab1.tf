bbab1eaea8f60a0a34f41a5d6e135c16fc112cd4
skhedim <46813029+skhedim@users.noreply.github.com>
2021-10-28 14:30:32 +0200

    Update lab1.tf

--- lab1.tf
+++ lab1.tf
@@ -1,17 +1,3 @@
-terraform {
-  backend "s3" {
-    bucket = "tfstate-skhedim"
-    key    = "mytfsate"
-    region = "us-east-1"
-  }
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-}
-
 # Configure the AWS Provider
 provider "aws" {
   region     = "us-east-1"
