f4c90857a5c1c4b05c6ea86c3fff2c5c77ce99a6
skhedim <46813029+skhedim@users.noreply.github.com>
2022-11-18 15:33:58 +0100

    Update tp1.tf

--- tp1.tf
+++ tp1.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_vpc" "main" {
-  cidr_block       = "10.0.0.0/16"
+  cidr_block = "10.0.0.0/16"
 
   tags = {
     Name = "cesi"
@@ -11,8 +11,8 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_subnet" "public-a" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
   availability_zone = "us-east-1a"
 
   tags = {
@@ -21,12 +21,32 @@ resource "aws_subnet" "public-a" {
 }
 
 resource "aws_subnet" "public-b" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.2.0/24"
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
   availability_zone = "us-east-1b"
-  
+
   tags = {
     Name = "public-b"
+  }
+}
+
+resource "aws_subnet" "private-a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "private-a"
+  }
+}
+
+resource "aws_subnet" "private-b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "private-b"
   }
 }
 
@@ -71,13 +91,17 @@ resource "aws_key_pair" "deployer" {
 }
 
 resource "aws_instance" "web" {
-  ami           = "ami-08c40ec9ead489470"
-  instance_type = "t2.micro"
+  ami                         = "ami-08c40ec9ead489470"
+  instance_type               = "t2.micro"
   associate_public_ip_address = true
-  subnet_id = aws_subnet.public-a.id
-  key_name = aws_key_pair.deployer.key_name
-  user_data = file("${path.root}/apache.sh")
-  vpc_security_group_ids = [aws_security_group.allow_http.id]
+  subnet_id                   = aws_subnet.public-a.id
+  key_name                    = aws_key_pair.deployer.key_name
+  vpc_security_group_ids      = [aws_security_group.allow_http.id]
+
+  user_data = templatefile("${path.root}/apache.sh", {
+    password = random_password.dbpassword.result
+    endpoint = aws_db_instance.dbWordPress.address
+  })
 
   tags = {
     Name = "wordpress"
@@ -90,23 +114,123 @@ resource "aws_security_group" "allow_http" {
   vpc_id      = aws_vpc.main.id
 
   ingress {
-    description      = "HTTP from VPC"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    description = "HTTP from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
     Name = "allow_http"
   }
+}
+
+resource "random_password" "dbpassword" {
+  length  = 16
+  special = false
+}
+
+resource "aws_security_group" "allow_rds" {
+  name        = "allow_rds"
+  description = "Allow mysql inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.allow_http.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_http-tf"
+  }
+}
+
+resource "aws_db_subnet_group" "rds" {
+  name       = "wordpress"
+  subnet_ids = [aws_subnet.private-a.id, aws_subnet.private-b.id]
+
+  tags = {
+    Name = "wordpress-rds"
+  }
+}
+
+resource "aws_db_instance" "dbWordPress" {
+  engine                 = "mysql"
+  engine_version         = "5.7"
+  allocated_storage      = 20
+  instance_class         = "db.t2.micro"
+  vpc_security_group_ids = [aws_security_group.allow_rds.id]
+  db_subnet_group_name   = aws_db_subnet_group.rds.name
+  name                   = "wordpress"
+  username               = "admin"
+  password               = random_password.dbpassword.result
+  skip_final_snapshot    = true
+
+  tags = {
+    Name = "WordPress DB"
+  }
+}
+
+resource "aws_nat_gateway" "example" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public-b.id
+
+  tags = {
+    Name = "gw NAT"
+  }
+
+  depends_on = [aws_internet_gateway.igw-tf]
+}
+
+resource "aws_eip" "nat" {
+  vpc      = true
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.epsi-tf.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.example.id
+  }
+
+  tags = {
+    Name = "private-tf"
+  }
+}
+
+resource "aws_route_table_association" "priva" {
+  subnet_id      = aws_subnet.private-a.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "privb" {
+  subnet_id      = aws_subnet.private-b.id
+  route_table_id = aws_route_table.private.id
 }
 
 output "public_ip" {
