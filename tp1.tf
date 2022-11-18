835c8a6a17649b09c2f2228257369959d7076819
skhedim <46813029+skhedim@users.noreply.github.com>
2022-11-18 10:45:42 +0100

    Update tp1.tf

--- tp1.tf
+++ tp1.tf
@@ -1,39 +1,45 @@
 provider "aws" {
-  region = "eu-west-1"
+  region = "us-east-1"
 }
 
-resource "aws_instance" "web" { // création de VM
-  ami           = "ami-0ee415e1b8b71305f" // OS pour la VM
-  instance_type = "t2.micro" // Memoire-CPU de la VM
-  subnet_id     = aws_subnet.main.id // Subnet créé ligne23
-  associate_public_ip_address = true //Attribution d'une IP publique
-  
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+
   tags = {
-    Name = "khedim" // Nom de la VM
+    Name = "cesi"
   }
 }
 
-data "aws_vpc" "selected" {
-}
-
-resource "aws_subnet" "main" {
-  vpc_id     = data.aws_vpc.selected.id
+resource "aws_subnet" "public-a" {
+  vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
 
   tags = {
-    Name = "subnet-a"
+    Name = "public-a"
+  }
+}
+
+resource "aws_subnet" "public-b" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+  
+  tags = {
+    Name = "public-b"
   }
 }
 
 resource "aws_internet_gateway" "gw" {
-  vpc_id = data.aws_vpc.selected.id
+  vpc_id = aws_vpc.main.id
+
   tags = {
-    Name = "igw-epsi"
+    Name = "gw-cesi"
   }
 }
 
 resource "aws_route_table" "example" {
-  vpc_id = data.aws_vpc.selected.id
+  vpc_id = aws_vpc.main.id
 
   route {
     cidr_block = "0.0.0.0/0"
@@ -41,11 +47,68 @@ resource "aws_route_table" "example" {
   }
 
   tags = {
-    Name = "public"
+    Name = "rt-cesi"
   }
 }
 
 resource "aws_route_table_association" "a" {
-  subnet_id      = aws_subnet.main.id
+  subnet_id      = aws_subnet.public-a.id
   route_table_id = aws_route_table.example.id
+}
+
+resource "aws_route_table_association" "b" {
+  subnet_id      = aws_subnet.public-b.id
+  route_table_id = aws_route_table.example.id
+}
+
+resource "tls_private_key" "example" {
+  algorithm = "RSA"
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "deploy-key"
+  public_key = tls_private_key.example.public_key_openssh
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-08c40ec9ead489470"
+  instance_type = "t2.micro"
+  associate_public_ip_address = true
+  subnet_id = aws_subnet.public-a.id
+  key_name = aws_key_pair.deployer.key_name
+  user_data = file("${path.root}/apache.sh")
+  vpc_security_group_ids = [aws_security_group.allow_http.id]
+
+  tags = {
+    Name = "wordpress"
+  }
+}
+
+resource "aws_security_group" "allow_http" {
+  name        = "allow_http_tf"
+  description = "Allow HTTP inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "HTTP from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_http"
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.web.public_ip
 }
